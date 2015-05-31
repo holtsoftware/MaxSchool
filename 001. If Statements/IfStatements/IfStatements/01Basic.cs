688d443f5baf52666392db2d7bbb9261aa5e4d95
Adam Holt <holtsoftware@outlook.com>
2015-05-30 22:06:47 -0600

    Started building lesson one

---
+++ 001. If Statements/IfStatements/IfStatements/01Basic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+	public class _01Basic
+	{
+		/// <summary>
+		/// This method takes a String <paramref name="number"/> and returns its number representation of it up to 10
+		/// i.e. if you send in "One" it will return 1
+		/// </summary>
+		/// <param name="number">The string number to parse</param>
+		/// <returns>0-10 depending on the number passed</returns>
+		public int ConvertToNumber(String number)
+		{
+			if(number == "Zero")
+			{
+				return 0;
+			}
+			// One
+			// Two
+			// Three
+			// Four
+			// Five
+			// Six
+			// Seven
+			// Eight
+			// Nine
+			// Ten
+
+			return -1; // number is not supported
+		}
+	}
+}
---
+++ 001. If Statements/IfStatements/IfStatements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+	class Program
+	{
+		/// <summary>
+		/// This is the entry point into this console application.
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			Console.Write("Begining Test of 01Basic");
+			_01Basic basic = new _01Basic();
+		}
+	}
+}
